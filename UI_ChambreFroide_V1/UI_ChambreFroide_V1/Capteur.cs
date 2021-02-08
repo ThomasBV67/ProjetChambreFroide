23efd26399a82105a1990d6e9b12d8df3cbcc583
Thomas <thomasbureauviens@gmail.com>
2021-02-08 10:03:13 -0500

    Ajout de commentaires, deletion de code innutile

--- UI_ChambreFroide_V1/UI_ChambreFroide_V1/Capteur.cs
+++ UI_ChambreFroide_V1/UI_ChambreFroide_V1/Capteur.cs
@@ -8,26 +8,20 @@ namespace UI_ChambreFroide_V1
 {
     /// <summary>
     /// Classe contenant les informations données par la table Capteurs de la db
+    /// Name : Nom donné par l'usager au capteur
+    /// Address : Addresse de 8 Bytes unique au capteur
+    /// Group : Nom de grouppage donné au capteur par l'usager
+    /// AlertLow : Température minimale permise avant qu'une alerte ne soit lancée
+    /// AlertHigh : Température maximale permise avant qu'une alerte ne soit lancée
+    /// Id : Id dans la dataBase
+    /// Module : # du module duquel vient le capteur
+    /// Index : Index du capteur dans le module
+    /// Set : Indique si le capteur à déja été setup par l'usager.
     /// </summary>
     public class Capteur
     {
         public String Name, Address, Group;
         public double AlertLow, AlertHigh;
         public int Id, Module, Index, Set;
-
-        /*
-        public Capteur(int idCap, String addrCap, int setCap, String nameCap, double alertLowCap, double alertHighCap, String groupCap, int moduleCap, int indexCap)
-        {
-            Id = idCap;
-            Address = addrCap;
-            Set = setCap;
-            Name = nameCap;
-            AlertLow = alertLowCap;
-            AlertHigh = alertHighCap;
-            Group = groupCap;
-            Module = moduleCap;
-            Index = indexCap;
-        }
-        */
     }
 }
