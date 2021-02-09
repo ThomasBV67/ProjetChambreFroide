3ff061f279ddf3d06d03512c98d4c600534b4580
Thomas Bureau-Viens <thomasbureauviens@gmail.com>
2021-02-09 15:19:15 -0500

    Ajout de la classe de température

---
+++ UI_ChambreFroide_V1/UI_ChambreFroide_V1/MesureTemp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI_ChambreFroide_V1
+{
+    class MesureTemp
+    {
+        int Id, Alert;
+        String TimeStamp, Capteur;
+        double Temperature;
+    }
+}
